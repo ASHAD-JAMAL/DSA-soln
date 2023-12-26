e7177b8b897b53177aa5c3ff5bd5eed573c60f91
Ashad Jamal <ashadjamalhyt@gmail.com>
2023-12-26 10:33:04 +0530

    26dec

---
+++ Problem/PowerOfFour342.java
@@ -0,0 +1,16 @@
+package Problem;
+
+public class PowerOfFour342 {
+    public boolean isPowerOfFour(int n) {
+        for(int i=0;i<=15;i++){
+            int powerOfFour=(int)Math.pow(4,i);
+            if(powerOfFour==n){
+                return true;
+            }
+            if(powerOfFour>n){
+                return false;
+            }
+        }
+        return false;
+    }
+}
