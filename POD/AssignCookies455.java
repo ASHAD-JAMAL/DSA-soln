60c8df171b8c414710236c87115e51e55a8cb620
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-01-01 10:48:07 +0530

    1jan

---
+++ POD/AssignCookies455.java
@@ -0,0 +1,23 @@
+package POD;
+
+import java.util.Arrays;
+
+public class AssignCookies455 {
+        public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        int i=0;
+        int j=0;
+        while(i<g.length && j<s.length){
+            if(g[i]<=s[j]){
+                count++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return count;
+    }
+}
