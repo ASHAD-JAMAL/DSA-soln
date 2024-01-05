5c5b3dc22c77761262a7e1a490239d630c3905d3
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-01-05 14:25:08 +0530

    5jan

---
+++ Problem/PowerofThree326.java
@@ -0,0 +1,15 @@
+package Problem;
+
+public class PowerofThree326 {
+    public boolean isPowerOfThree(int n) {
+        if(n<0){
+            return false;
+        }
+        for(int i=0;i<=30;i++){
+            if(Math.pow(3,i)==n){
+                return true;
+            }
+        }
+        return false;
+    }
+}
