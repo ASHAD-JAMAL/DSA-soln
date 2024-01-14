0bfce8feeb2a35500a9c8ea6a35486ef6893f3ea
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-01-14 21:25:33 +0530

    14jan

---
+++ LinkedList/ReverseLL206.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class ReverseLL206 {
+    // public ListNode reverseList(ListNode head) {
+    //     if(head==null || head.next==null){
+    //         return head;
+    //     }
+    //     ListNode prev=null;
+    //     ListNode curr=head;
+    //     while(curr!=null){
+    //         ListNode forward=curr.next;
+    //         curr.next=prev;
+    //         prev=curr;
+    //         curr=forward;
+    //     }
+    //     return prev;
+    // }
+}
